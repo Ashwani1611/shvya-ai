99072f0cb00094221d295c3385a615d8edadb807
Ashwani1611 <142573202+Ashwani1611@users.noreply.github.com>
2026-09-06 04:08:39 +0530

    Register Auto Follow-ups conversation signals

--- apps/followups/apps.py
+++ apps/followups/apps.py
@@ -2,6 +2,9 @@ from django.apps import AppConfig
 
 
 class FollowupsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.followups'
-    label = 'followups'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.followups"
+    label = "followups"
+
+    def ready(self):
+        from . import signals  # noqa: F401
