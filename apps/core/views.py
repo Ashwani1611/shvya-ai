239ca6c95fd356c20e7a0e464022b7796c2383a3
Ashwani1611 <142573202+Ashwani1611@users.noreply.github.com>
2026-09-06 01:50:19 +0530

    Share CRM user context with pricing page

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,10 +3,18 @@ from django.views.generic import TemplateView
 from apps.crm.authentication import get_crm_authenticated_user
 
 
-class HomeView(TemplateView):
-    template_name = "home.html"
+class CRMUserContextMixin:
+    """Expose the dedicated CRM user to public marketing templates."""
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["crm_user"] = get_crm_authenticated_user(self.request)
         return context
+
+
+class HomeView(CRMUserContextMixin, TemplateView):
+    template_name = "home.html"
+
+
+class PricingView(CRMUserContextMixin, TemplateView):
+    template_name = "pricing.html"
