d14da5b136c0324b16b395f33107801d8205af77
Ashwani1611 <142573202+Ashwani1611@users.noreply.github.com>
2026-09-06 01:24:26 +0530

    Expose CRM user on landing page

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,12 @@
 from django.views.generic import TemplateView
 
+from apps.crm.authentication import get_crm_authenticated_user
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["crm_user"] = get_crm_authenticated_user(self.request)
+        return context
