6a5c96383456a1412008e1f80387ae25127496d3
Ashwani1611 <142573202+Ashwani1611@users.noreply.github.com>
2026-09-07 02:01:58 +0530

    Configure hosted automation app

---
+++ apps/hosted_automation/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HostedAutomationConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.hosted_automation"
+    verbose_name = "Hosted WhatsApp Automation"
