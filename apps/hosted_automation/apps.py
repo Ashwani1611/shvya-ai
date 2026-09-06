a28efcd7e318d02332d9fd3b41232e45d5252ba7
Ashwani1611 <142573202+Ashwani1611@users.noreply.github.com>
2026-09-07 02:09:53 +0530

    Install Hosted Account AI delay dispatcher

--- apps/hosted_automation/apps.py
+++ apps/hosted_automation/apps.py
@@ -5,3 +5,8 @@ class HostedAutomationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.hosted_automation"
     verbose_name = "Hosted WhatsApp Automation"
+
+    def ready(self):
+        from services.channels.hosted_ai_delay import install_hosted_ai_delay_dispatch
+
+        install_hosted_ai_delay_dispatch()
