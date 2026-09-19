7576b1155e79d7f22d2e918c6bdaba1c48444007
Ashwani1611 <142573202+Ashwani1611@users.noreply.github.com>
2026-09-19 10:29:22 +0530

    Add diagnostic read capability to API keys

---
+++ apps/organizations/migrations/0005_apikey_can_read_diagnostics.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("organizations", "0004_organizationpayment"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="apikey",
+            name="can_read_diagnostics",
+            field=models.BooleanField(default=False),
+        ),
+    ]
